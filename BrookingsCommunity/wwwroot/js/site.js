c5dcefe85c3940c1d38e1a6ca9a90c75072a3377
PatrickSBJ <behrensjungferp@my.lanecc.edu>
2019-10-26 14:06:03 -0700

    beta version

--- BrookingsCommunity/wwwroot/js/site.js
+++ BrookingsCommunity/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+var colors = ["", "#d7d7e9", "orange", "#e0e9d7", "white",
+    "#e9d7e0"];
+
+setInterval(changeBackgroundColor, 5000);
+
+function changeBackgroundColor()
+{
+    document.body.style.backgroundColor = colors[Math.floor((Math.random() * 5) + 1)]
+}
