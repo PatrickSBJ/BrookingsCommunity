034e0adeb27691300d03e3eed6406c044c6663a5
PatrickSBJ <behrensjungferp@my.lanecc.edu>
2019-11-11 13:39:52 -0800

    Part two of lab are in TestController

---
+++ BrookingsCommunity/Controllers/TestController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BrookingsCommunity.Controllers
+{
+    public class TestController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+        public IActionResult PageOne()
+        {
+            return Content("Demo");
+        }
+        public String PageTwo()
+        {
+            return "Test string";
+        }
+        public StatusCodeResult PageThree()
+        {
+            return StatusCode(418);
+        }
+        public FileResult PageFour()
+        {
+            return File(@"wwwroot\test.txt", "test.txt");
+        }
+    }
+}
