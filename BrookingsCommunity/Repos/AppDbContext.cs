220e1baea210050df37650f2f2f859cab303e81d
PatrickSBJ <behrensjungferp@my.lanecc.edu>
2019-12-04 19:40:44 -0800

    s

---
+++ BrookingsCommunity/Repos/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BrookingsCommunity.Models;
+
+namespace BrookingsCommunity.Repos
+{
+    public class DbContext : DbContext
+    {
+        public DbContext(
+          DbContextOptions<DbContext> options) : base(options) { }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<ImportantLocation> ImportantLocations { get; set; }
+        public DbSet<ImportantPerson> ImportantPeople { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Reply> Replies { get; set; }
+    }
+}
